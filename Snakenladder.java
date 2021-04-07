031c0ac5576b0e9f368be0bf499699193e4a189b
Naveen <naveentruly86@gmail.com>
2021-04-07 15:28:37 +0530

    Added: Check for option

--- Snakenladder.java
+++ Snakenladder.java
@@ -7,9 +7,30 @@ import java.util.*;
       System.out.println("Welcome To Snake and Ladder Game");
 
        int Startpoint = 0;
+       int player1 = Startpoint;
        Random rand = new Random();
        int diceroll = rand.nextInt(6)+1;
 
        System.out.println(diceroll);
+
+       Random opt = new Random();
+       int position = opt.nextInt(3);
+
+       switch(position){
+
+                   case 0:
+                             player1 = player1;
+                             System.out.println("Sorry,u cant play remain in same position");
+                             break;
+                   case 1:
+                             player1 = player1+diceroll;
+                             System.out.println(" Yahoo! you got ladder " + diceroll + " steps move forward ");
+                             break;
+                   case 2:
+                             player1 = player1-diceroll;
+                             System.out.println(" Hiss! you are beaten by snake " + diceroll + " steps move backward ");
+                             break;
+         }
+     System.out.println("Player position: " + player1);
   }
 }
