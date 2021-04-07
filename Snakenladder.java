aebf226153488d6c1b8dbc8d300919595a444afd
Naveen <naveentruly86@gmail.com>
2021-04-07 14:47:55 +0530

    Added: Roll the Dice

--- Snakenladder.java
+++ Snakenladder.java
@@ -1,8 +1,15 @@
-public class Snakenladder{
+import java.util.*;
+
+
+  public class Snakenladder{
 
     public static void main(String[] args){
       System.out.println("Welcome To Snake and Ladder Game");
 
        int Startpoint = 0;
+       Random rand = new Random();
+       int diceroll = rand.nextInt(6)+1;
+
+       System.out.println(diceroll);
   }
 }
